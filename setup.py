d5d50e1d081c1b554e123728fa5a3a9c28c40b73
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2021-02-23 16:03:14 -0700

    updated  for pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="googlecontroller",
-    version="3.0.5",
+    version="3.0.6",
     description="control your google home",
     long_description=README,
     long_description_content_type="text/markdown",
