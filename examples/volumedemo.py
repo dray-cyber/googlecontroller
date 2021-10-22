1f5f92011d9dd7326dd9cd53b4f07d7ccf6cdf4e
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2021-10-22 12:34:38 -0700

    fixes

--- examples/volumedemo.py
+++ examples/volumedemo.py
@@ -2,5 +2,6 @@ from googlecontroller import GoogleAssistant
 
 host = "192.168.0.4" # your google assistant ip this is mine
 home = GoogleAssistant(host=host)
-print("Type Your Volume 0-100")
-home.volume(volumelevel=input())
+while True:
+    print("Type Your Volume 0-100")
+    home.volume(volumelevel=int(input()))
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="googlecontroller",
-    version="5.7",
+    version="5.8",
     description="control your google home",
     long_description=README,
     long_description_content_type="text/markdown",
